405681a8aa3c48d73ce85184df0912adfb95e989
jawebb-school <jawebb@g.clemson.edu>
2022-12-15 21:33:34 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,9 @@
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum -y install nodejs npm
+yum update
+yum install ruby
+yum install wget
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
